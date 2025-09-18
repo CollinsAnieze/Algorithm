d929ab6d97301b5e1a3e42ad44b7c7e480f510e3
collins Anieze <chigoziecollins434@gmail.com>
2025-09-18 13:22:22 +0100

    Completed MakeComplete Dna strand algorithm.

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,4 +1,5 @@
-﻿using Algorithm.Arrays;
+﻿using Algorithm.CodeWars;
 
-Console.WriteLine();
+Console.WriteLine(StringKatas.MakeComplement("GTAT"));
+Console.WriteLine(StringKatas.MakeComplement("GTAT") == "CATA");
 Console.ReadLine();
